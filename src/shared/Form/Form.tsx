b1366d791bc23925f1c255e95c1476ce40d2ac1d
s-salomaha <s.salomaha@gmail.com>
2022-09-03 19:52:20 +0300

    12.2 State of fields. Different approaches

--- src/shared/Form/Form.tsx
+++ src/shared/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useEffect, useRef } from 'react';
+import React, {ChangeEvent, FormEvent, useEffect, useRef, useState} from 'react';
 import styles from './form.scss';
 
 interface IFormProps {
@@ -10,9 +10,16 @@ interface IFormProps {
 
 export function Form({ textareaTagProps, setFocus, setAuthorNameByFocus = false, authorName = '' }: IFormProps) {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [value, setValue] = useState('');
+  const [valueTouched, setValueToched] = useState(false);
+  const [valueError, setValueError] = useState('');
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
+  }
+
+  function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setValue(event.target.value);
   }
 
   useEffect(() => {
@@ -24,7 +31,8 @@ export function Form({ textareaTagProps, setFocus, setAuthorNameByFocus = false,
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <textarea className={styles.input} ref={textAreaRef} {...textareaTagProps} />
+      {/*<textarea className={styles.input} ref={textAreaRef} {...textareaTagProps} />*/}
+      <textarea className={styles.input} value={value} onChange={handleChange} aria-invalid={valueError ? 'true' : undefined} />
       <div className={styles.formFooter}>
         <button type="submit" className={styles.button}>Комментировать</button>
       </div>

0bd4d4a1c756b3f742310938832e76b8c6b9c94c
s-salomaha <s.salomaha@gmail.com>
2022-09-03 20:40:12 +0300

    12.4 Save state of form and validate fields

--- src/shared/Form/Form.tsx
+++ src/shared/Form/Form.tsx
@@ -11,15 +11,27 @@ interface IFormProps {
 export function Form({ textareaTagProps, setFocus, setAuthorNameByFocus = false, authorName = '' }: IFormProps) {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const [value, setValue] = useState('');
-  const [valueTouched, setValueToched] = useState(false);
+  const [formTouched, setFormToched] = useState(false);
   const [valueError, setValueError] = useState('');
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
+    setFormToched(true);
+    setValueError(validateValue());
+
+    const isFormValid = !validateValue();
+    if (!isFormValid) return;
+
+    alert('Форма отправлена!');
   }
 
   function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
     setValue(event.target.value);
+  }
+
+  function validateValue() {
+    if (value.length <= 3) return 'Введите больше 3-х символов';
+    return '';
   }
 
   useEffect(() => {
@@ -32,7 +44,13 @@ export function Form({ textareaTagProps, setFocus, setAuthorNameByFocus = false,
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       {/*<textarea className={styles.input} ref={textAreaRef} {...textareaTagProps} />*/}
-      <textarea className={styles.input} value={value} onChange={handleChange} aria-invalid={valueError ? 'true' : undefined} />
+      <textarea
+        className={styles.input}
+        value={value}
+        onChange={handleChange}
+        aria-invalid={valueError ? 'true' : undefined}
+      />
+      {formTouched && valueError && (<div>{valueError}</div>)}
       <div className={styles.formFooter}>
         <button type="submit" className={styles.button}>Комментировать</button>
       </div>

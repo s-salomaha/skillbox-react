8a126370462239451347d8758be76ae96fb1c427
s-salomaha <s.salomaha@gmail.com>
2022-09-19 22:33:57 +0300

    Add Welcome component

---
+++ src/shared/Welcome/Welcome.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './welcome.scss';
+
+export function Welcome() {
+  return (
+    <div className={styles.welcome}>
+      First log in
+    </div>
+  );
+}
